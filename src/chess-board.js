b505dd4115c29edfa045614218a515a87d34e197
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 12:01:25 -0600

    Move Node factory function to its own module

--- src/chess-board.js
+++ src/chess-board.js
@@ -1,23 +1,4 @@
-const Node = (coords) => {
-  const data = coords;
-  const prev = null;
-  const next = null;
-  const parent = null;
-  const c1 = null;
-  const c2 = null;
-  const c3 = null;
-  const c4 = null;
-  const c5 = null;
-  const c6 = null;
-  const c7 = null;
-  const c8 = null;
-
-  return {
-    data, prev, next, c1, c2, c3, c4, c5, c6, c7, c8, parent,
-  };
-};
-
-export { Node };
+import Node from './node';
 
 const Board = (() => {
   const head = Node([0, 0]);
---
+++ src/node.js
@@ -0,0 +1,20 @@
+const Node = (coords) => {
+  const data = coords;
+  const prev = null;
+  const next = null;
+  const parent = null;
+  const c1 = null;
+  const c2 = null;
+  const c3 = null;
+  const c4 = null;
+  const c5 = null;
+  const c6 = null;
+  const c7 = null;
+  const c8 = null;
+
+  return {
+    data, prev, next, c1, c2, c3, c4, c5, c6, c7, c8, parent,
+  };
+};
+
+export default Node;
